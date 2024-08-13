7e2c4d3e8d1b4f6bfb5eacbb4117541939ff5689
Angelina <angelaaquin13@gmail.com>
2024-08-13 00:46:40 +0000

    initial

--- exercises/07.2-Letter-Counter/app.js
+++ exercises/07.2-Letter-Counter/app.js
@@ -1,6 +1,17 @@
-let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
+ let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
+
 let counts = {};
 
 // Your code here
-
-console.log(counts);
+let lower = par.toLowerCase();
+for(let i=0; i< lower.length;i++){
+    if(lower[i] == " ") {}
+    else if(!(lower[i] in counts)){
+        counts[lower[i]] = 1
+    }
+    else{
+        counts[lower[i]]++; 
+    }
+}
+ console.log(counts);
+ 
